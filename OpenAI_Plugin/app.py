701daff038d2099e4b1c4e69c55f79bd90fd12a8
David <104325852+Cdaprod@users.noreply.github.com>
2023-05-16 12:22:03 -0400

    Initial commit

---
+++ OpenAI_Plugin/app.py
@@ -0,0 +1,37 @@
+# import os
+
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route('/generate_project', methods=['POST'])
+# def generate_project():
+#     data = request.get_json()
+#     project_name = data['project_name']
+#     code = data['code']
+
+#     os.makedirs(project_name, exist_ok=True)
+#     with open(f"{project_name}/main.py", "w") as file:
+#         file.write(code)
+#     return {"status": "success"}, 200
+
+import os
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/generate_project', methods=['POST'])
+def generate_project():
+    data = request.get_json()
+    project_name = data['project_name']
+    code = data['code']
+
+    os.makedirs(project_name, exist_ok=True)
+    with open(f"{project_name}/main.py", "w") as file:
+        file.write(code)
+
+    return {"status": "success"}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
