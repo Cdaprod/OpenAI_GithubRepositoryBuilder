065245f611d5266ddabc8ec3eb27115dadac0a7c
David <104325852+Cdaprod@users.noreply.github.com>
2023-05-16 12:24:39 -0400

    Update app.py
    Cleaned up Flask API

--- OpenAI_Plugin/app.py
+++ OpenAI_Plugin/app.py
@@ -1,20 +1,3 @@
-# import os
-
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# @app.route('/generate_project', methods=['POST'])
-# def generate_project():
-#     data = request.get_json()
-#     project_name = data['project_name']
-#     code = data['code']
-
-#     os.makedirs(project_name, exist_ok=True)
-#     with open(f"{project_name}/main.py", "w") as file:
-#         file.write(code)
-#     return {"status": "success"}, 200
-
 import os
 
 from flask import Flask, request
@@ -27,11 +10,12 @@ def generate_project():
     project_name = data['project_name']
     code = data['code']
 
-    os.makedirs(project_name, exist_ok=True)
-    with open(f"{project_name}/main.py", "w") as file:
+    os.makedirs(f"Workspace/{project_name}", exist_ok=True)
+    with open(f"Workspace/{project_name}/main.py", "w") as file:
         file.write(code)
 
     return {"status": "success"}, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
